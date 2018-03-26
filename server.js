a723ba55ae1c827c2c81968a15889e32bf5485d1
Michael Pabon <michael@Michaels-MacBook-Pro.local>
2018-03-26 03:13:52 -0400

    Integrate Landing Page and Diary with Node Backend

--- server.js
+++ server.js
@@ -1,31 +1,39 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var path = require('path');
 
 var app = express();
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }));
-var Entry = require('./models/entry');
+app.use('/', require('./controllers/landing-static'));
+app.use('/api/entries', require('./controllers/api/entries'));
+app.use('/diary', require('./controllers/diary-static'));
 
-app.get('/api/entries', function(req, res, next){
-    Entry.find(function(err, entries){
-        if(err) { return next(err); }
-        res.json(entries);
-    });
+
+//***************************************************************************
+//***This is the current workaround for serving what's in the 'src' folder***
+//Serve CSS
+app.get('/src/css/landingPage.css', function(req,res){
+    res.sendFile(path.join(__dirname, '/src/css', 'landingPage.css'));
 });
-
-
-app.post('/api/entries', function(req, res, next){
-    var entry = new Entry({
-        description: req.body.description,
-        amount: req.body.amount,
-        unit: req.body.unit,
-        calories: req.body.calories
-    });
-    entry.save(function(err, entry){
-        if(err){ return next(err); }
-        res.status(201).json(entry);
-    });
+app.get('/src/css/diary.css', function(req,res){
+    res.sendFile(path.join(__dirname, '/src/css', 'diary.css'));
 });
+app.get('/src/css/global.css', function(req, res){
+    res.sendFile(path.join(__dirname, '/src/css', 'global.css'));
+});
+//Serve Js
+app.get('/src/js/global.js', function(req,res){
+    res.sendFile(path.join(__dirname, '/src/js', 'global.js'));
+});
+app.get('/src/js/landingPage.js', function(req,res){
+    res.sendFile(path.join(__dirname, '/src/js', 'landingPage.js'));
+});
+//Serve Multiedia
+app.get('/assets/background.jpg', function(req,res){
+    res.sendFile(path.join(__dirname, '/assets', 'background.jpg'));
+});
+//***************************************************************************
 
 app.listen(3000, function(){
     console.log('Server Listening on', 3000);

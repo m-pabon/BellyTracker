f65b236947015ff8d9bae867a2af2b69df53b67f
Michael Pabon <michael@Michaels-MacBook-Pro.local>
2018-03-26 04:03:59 -0400

    Added db.js reminder

--- db.js
+++ db.js
@@ -1,5 +1,6 @@
 var mongoose = require('mongoose');
 
+//BEFORE PUSHING EMPTY THE PASSWORD FIELD
 const mongoDbUrl = 'mongodb+srv://admin:<PASSWORD>@bellytracker-rcy2p.mongodb.net/test';
 
 mongoose.connect(mongoDbUrl, {dbName: 'test'}, function(){

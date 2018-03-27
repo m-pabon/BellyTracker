e7a595cd1548e11b13bb44e77e5d7a68577e4d74
Michael Pabon <michael@Michaels-MacBook-Pro.local>
2018-03-27 03:24:15 -0400

    Gulp Config Cont.: Removed server.js serving background.js code

--- server.js
+++ server.js
@@ -11,12 +11,6 @@ app.use('/', require('./controllers/landing-static'));
 app.use('/api/entries', require('./controllers/api/entries'));
 app.use('/diary', require('./controllers/diary-static'));
 
-//Serve Multiedia
-app.get('/assets/background.jpg', function (req, res) {
-    res.sendFile(path.join(__dirname, '/assets', 'background.jpg'));
-});
-//***************************************************************************
-
 app.listen(3000, function () {
     console.log('Server Listening on', 3000);
 });

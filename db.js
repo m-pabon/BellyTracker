c6b4bc9a766f8a1b3a23f4f69c5b483544ae48f5
Michael Pabon <mpabon@outlook.com>
2018-04-11 03:04:52 -0400

    Fixed client-authentication trouble. Erros still persist with entry authentication

--- db.js
+++ db.js
@@ -1,7 +1,7 @@
 var mongoose = require('mongoose');
 
 //BEFORE PUSHING EMPTY THE PASSWORD FIELD
-const mongoDbUrl = 'mongodb+srv://admin:A7x0918%21@bellytracker-rcy2p.mongodb.net/test';
+const mongoDbUrl = 'mongodb+srv://admin:<PASSWORD>@bellytracker-rcy2p.mongodb.net/test';
 
 mongoose.connect(mongoDbUrl, {
     dbName: 'test'
